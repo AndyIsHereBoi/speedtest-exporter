7bc9a3d9a1f14dbef49af84748e25bdfa89a5ebf
AndyIsHereBoi <69745145+AndyIsHereBoi@users.noreply.github.com>
2022-06-17 16:48:00 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import speedtest
+s = speedtest.Speedtest()
+
+print("Test Download Speed...")
+
+download_result = s.download()/1025/1024
+print(f"Your download speed is:{download_result:.2f}mbit/s")
+
+print("Test Upload Speed...")
+
+upload_result = s.upload()/1024/1024
+print(f"Your upload speed is:{upload_result:.2f}mbit/s")
+
+print("Test Ping Test...")
+
+ping_result = s.result.ping
+print(f"Your ping is:{ping_result}ms")
