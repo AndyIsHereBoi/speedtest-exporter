c6b7b0e02ef0887b7f0466dc5950f8ef3993b51c
AndyIsHereBoi <69745145+AndyIsHereBoi@users.noreply.github.com>
2022-06-17 16:52:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ s = speedtest.Speedtest()
 print("Test Download Speed...")
 
 download_result = s.download()/1025/1024
-print(f"Your download speed is:{download_result:.2f}mbit/s")
+print(f"Your download speed is:{download_result}mbit/s")
 
 print("Test Upload Speed...")
 
 upload_result = s.upload()/1024/1024
-print(f"Your upload speed is:{upload_result:.2f}mbit/s")
+print(f"Your upload speed is:{upload_result}mbit/s")
 
 print("Test Ping Test...")
 
 ping_result = s.result.ping
 print(f"Your ping is:{ping_result}ms")
+
+print(s.result)
